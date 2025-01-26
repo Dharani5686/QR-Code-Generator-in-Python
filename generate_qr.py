bda6a402a5ca198ab1f2fbd94a26fe876d5bd94c
Dharani <91747625+Dharani5686@users.noreply.github.com>
2025-01-26 10:38:28 +0530

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -1,3 +1,3 @@
 import qrcode as qr
-img=qr.make("https://www.youtube.com/@shivajitv")
-img.save("Vikram's_youtube.png")
+img=qr.make("https://www.linkedin.com/in/dharani-katkoju-8505051a9/")
+img.save("Dharani's_Linkedin_profile.png")
